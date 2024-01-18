f1d8cc61d3349837fe3c298cd7daf3e5bde5cb44
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2024-01-18 10:27:07 +0100

    reading full file with rdbuf and added string replacement method

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 21:44:15 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/01/18 09:50:38 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/01/18 10:24:44 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,38 @@
 #include <fstream>
 #include <sstream>
 
+std::string	replaceStrings(std::string source, std::string stringToReplace, std::string stringNew)
+{
+	size_t		pos;
+	size_t		stringLength;
+
+	stringLength = source.length();
+	pos = 0;
+	while (pos < stringLength)
+	{
+		pos = source.find(stringToReplace, pos);
+		if (pos == std::string::npos)
+			break ;
+		source.erase(pos, stringToReplace.length());
+		source.insert(pos, stringNew);
+		pos += stringNew.length();
+	}
+	return (source);
+}
+
 int main(int argc, char **argv)
 {
-	std::stringstream fileContent;
-	std::string outputContent = "this is the output content";
+	std::string			fileContent;
+	std::stringstream	buffer;
 
 	if (argc != 4)
 	{
 		std::cout << "please input like: ./replacer filename string1 string2" << std::endl;
+		return (1);
+	}
+	if (std::string(argv[2]) == "")
+	{
+		std::cout << "i cannot replace and an empty string" << std::endl;
 		return (1);
 	}
 	std::ifstream inputfile(argv[1]);
@@ -30,18 +54,15 @@ int main(int argc, char **argv)
 		std::cout << "cannot open input file" << std::endl;
 		return (1);
 	}
-	std::ofstream outputfile;
-	outputfile.open((std::string(argv[1]) + ".replace"), std::ios_base::app);
+	std::ofstream outputfile(std::string(argv[1]) + ".replace");
 	if (!outputfile.is_open())
 	{
 		std::cout << "error outputfile" << std::endl;
 		return (1);
 	}
-	while (std::getline(inputfile, fileContent, '\n'))
-	{
-		// std::cout << fileContent << std::endl;
-		// outputfile << fileContent;
-	}
+	buffer << inputfile.rdbuf();
+	fileContent = buffer.str();
+	fileContent = replaceStrings(fileContent, argv[2], argv[3]);
 	outputfile << fileContent;
 	inputfile.close();
 	outputfile.close();
