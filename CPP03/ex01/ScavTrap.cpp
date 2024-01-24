19652f354e4b143bc6919a1f1dfa52b45b3ba1a4
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2024-01-24 11:44:26 +0100

    first tests with heritage

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,9 +6,29 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 21:45:14 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/01/23 21:45:27 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/01/24 09:55:56 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
+ScavTrap::ScavTrap()
+: ClapTrap()
+, m_hitPoints(100)
+, m_energyPoints(50)
+, m_attackDamage(20)
+{
+}
+
+ScavTrap::ScavTrap(std::string name)
+: m_name(name)
+, m_hitPoints(100)
+, m_energyPoints(50)
+, m_attackDamage(20)
+{
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << m_name << " is now in gate keeper mode!" << std::endl;
+}
--- CPP03/ex01/ScavTrap.hpp
+++ CPP03/ex01/ScavTrap.hpp
@@ -6,16 +6,26 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 21:45:29 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/01/23 21:50:04 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/01/24 10:09:38 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
+# include <iostream>
+# include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap
 {
-	
+	private:
+		
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(ScavTrap &copy);
+		ScavTrap &operator =(ScavTrap &source);
+		~ScavTrap();
+		void guardGate();
 };
 
 #endif
