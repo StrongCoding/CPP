67e14a196accd854993743b67e708b069ba9c803
dnebatz <dnebatz@student.42wolfsburg.de>
2024-02-05 18:10:12 +0100

    fixed header

--- CPP04/ex00/Cat.hpp
+++ CPP04/ex00/Cat.hpp
@@ -6,9 +6,19 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:17:59 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/02/05 11:18:13 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/02/05 17:18:36 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
 
+#ifndef CAT_HPP
+# define CAT_HPP
+
+# include "Animal.hpp"
+
+class Cat : public Animal
+{
+	
+};
+
+#endif
