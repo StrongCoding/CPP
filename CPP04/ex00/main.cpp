715e576ef9befb1aa4b5627aab2a63290d12b900
dnebatz <dnebatz@student.42wolfsburg.de>
2024-02-06 08:49:50 +0100

    changed test cases

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 10:53:54 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/02/05 21:32:48 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/02/06 08:49:47 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,31 @@
 
 int main()
 {
-	const WrongAnimal* mutu = new WrongAnimal();
-	const WrongAnimal* a = new WrongCat();
-	std::cout << a->getType() << " a type" << std::endl;
-	a->makeSound(); //will not output the cat sound!
-	mutu->makeSound();
-
+	// const WrongAnimal* mutu = new WrongAnimal();
+	// const WrongAnimal* a = new WrongCat();
+	// std::cout << "a->gettype: "<< a->getType()  << std::endl;
+	// std::cout << "a->makeSound: ";
+	// a->makeSound(); //will not output the cat sound!
+	// std::cout << "mutu->makeSound: ";
+	// mutu->makeSound();
+	// delete mutu;
+	// delete a;
 
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	std::cout << j->getType() << " j type" << std::endl;
-	std::cout << i->getType() << " i type" << std::endl;
+	std::cout << " j->getType: " << j->getType()  << std::endl;
+	std::cout << " i->getType: " << i->getType() << std::endl;
+	std::cout << "i->makeSound: ";
 	i->makeSound(); //will output the cat sound!
+	std::cout << "j->makeSound: ";
 	j->makeSound();
+	std::cout << "meta->makeSound: ";
 	meta->makeSound();
 
-	// delete mutu;
-	// delete a;
-	// delete meta;
-	// delete j;
-	// delete i;
+	delete meta;
+	delete j;
+	delete i;
 
 	return (0);
 }
