a81e3264a50e6e101081ab41be622e6370820dab
dnebatz <dnebatz@student.42wolfsburg.de>
2024-02-19 13:41:34 +0100

    switched to main and added so see which is used

--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -6,13 +6,12 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:15:19 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/02/19 09:31:35 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/02/19 13:37:29 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
-# include <cstdlib>
 
 class Base
 {
@@ -24,9 +23,6 @@ class Base
 		Base(const Base &other);
 		Base &operator=(const Base &source);
 		virtual ~Base();
-		Base *generate(void);
-		void identify(Base* p);
-		void identify(Base& p);
 };
 
 #endif
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:14:45 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/02/19 10:41:10 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/02/19 13:38:33 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,89 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+Base *generate(void)
+{
+	std::srand(std::time(0));
+	int randomNumber = rand() % 3;
+	if (randomNumber == 0)
+		return new A();
+	else if (randomNumber == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p) != NULL)
+		std::cout << "pA" << std::endl;
+	else if (dynamic_cast<B *>(p) != NULL)
+		std::cout << "pB" << std::endl;
+	else if (dynamic_cast<C *>(p) != NULL)
+		std::cout << "pC" << std::endl;
+	else
+		std::cout << "pSomething else" << std::endl;
+}
+
+void identify(Base &p)
+{
+	bool found = false;
+	try 
+	{
+		A &test = dynamic_cast<A &>(p);
+		std::cout << "rA" << std::endl;
+		found = true;
+		(void)test;
+	}
+	catch(const std::exception &bc)
+	{
+	}
+	try
+	{
+		B &test = dynamic_cast<B &>(p);
+		std::cout << "rB" << std::endl;
+		found = true;
+		(void)test;
+	}
+	catch(const std::exception &bc)
+	{
+	}
+	try
+	{
+		C &test = dynamic_cast<C &>(p);
+		std::cout << "rC" << std::endl;
+		found = true;
+		(void)test;
+	}
+	catch(const std::exception &bc)
+	{
+	}
+	if (!found)
+		std::cout << "rSomething else" << std::endl;
+}
+
 
 int main(void)
 {
 	Base *base = new Base();
-	Base *random = base->generate();
-	base->identify(*random);
-	base->identify(random);
+	Base *random = generate();
+	identify(*random);
+	identify(random);
 	Base *a = new A();
 	Base *b = new B();
 	Base *c = new C();
-	base->identify(a);
-	base->identify(*a);
-	base->identify(b);
-	base->identify(*b);
-	base->identify(c);
-	base->identify(*c);
-	base->identify(NULL);
-	base->identify(base);
-	base->identify(*base);
+	identify(a);
+	identify(*a);
+	identify(b);
+	identify(*b);
+	identify(c);
+	identify(*c);
+	identify(NULL);
+	identify(base);
+	identify(*base);
 	delete random;
 	delete base;
 	delete a;
