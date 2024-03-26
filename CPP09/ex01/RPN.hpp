15025c9d5d2b0b41da391a8c5112ca204f22947d
dnebatz <dnebatz@student.42wolfsburg.de>
2024-03-26 09:50:45 +0100

    made private

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 08:13:19 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/03/19 09:07:52 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/03/26 09:35:37 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ class RPN
 		double	m_result;
 		std::stack<double> m_stack;
 		RPN();
+		RPN(RPN &other);
+		RPN &operator=(RPN &other);
 	public:	
 		RPN(std::string input);
 		~RPN();
-		RPN(RPN &other);
-		RPN &operator=(RPN &other);
+
 };
 
 #endif
