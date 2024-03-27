8566ec7ffacdcc0f21eed351272332e4459fc126
dnebatz <dnebatz@student.42wolfsburg.de>
2024-03-27 08:49:28 +0100

    implemented deque stack sorting

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -33,9 +33,12 @@ PmergeMe::PmergeMe(std::deque<unsigned int> &dequeStack)
 		pairs.push_back(std::make_pair(first, second));
 		it++;
 	}
-
-	// 2. sort pairs
-	// sortEachPair()
+	// PmergeMe::printPairs(pairs);
+	PmergeMe::sortEachPair(pairs);
+	// PmergeMe::printPairs(pairs);
+	PmergeMe::sortPairs(pairs);
+	// PmergeMe::printPairs(pairs);
+	PmergeMe::mergeInsertion(pairs, dequeStack);
 	
 }
 
@@ -63,6 +66,7 @@ PmergeMe::PmergeMe(std::vector<unsigned int> &vectorStack)
 	PmergeMe::sortPairs(pairs);
 	// PmergeMe::printPairs(pairs);
 	PmergeMe::mergeInsertion(pairs, vectorStack);
+	// PmergeMe::printStack(vectorStack);
 
 }
 
@@ -200,3 +204,109 @@ void PmergeMe::setNextJacNumber(unsigned int &prevJacNumber, unsigned int &jacNu
 	prevJacNumber = temp;
 }
 
+void PmergeMe::printStack(std::deque<unsigned int> &stack)
+{
+	std::deque<unsigned int>::iterator it_begin = stack.begin();
+	std::deque<unsigned int>::iterator it_end = stack.end();
+	
+	// std::cout << "before: ";
+	while (it_begin != it_end)
+	{
+		std::cout << *it_begin;
+		it_begin++;
+		if (it_begin != it_end)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printPairs(std::deque<std::pair<unsigned int, unsigned int> > &pairs)
+{
+	for (size_t i = 0; i < pairs.size(); ++i)
+	{
+		std::cout << "Pair " << i+1 << ": " << pairs[i].first << ", " << pairs[i].second << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::sortEachPair(std::deque<std::pair<unsigned int, unsigned int> > &pairs)
+{
+	unsigned int	temp;
+	for (size_t i = 0; i < pairs.size(); ++i)
+	{
+		// std::cout << "comparing first " << pairs[i].first << " second: " << pairs[i].second << std::endl;
+		if (pairs[i].first < pairs[i].second)
+			continue;
+		temp = pairs[i].first;
+		pairs[i].first = pairs[i].second;
+		pairs[i].second = temp;
+		// std::cout << "swapping first " << temp << " second: " << pairs[i].first << std::endl;
+	}
+}
+
+
+void PmergeMe::sortPairs(std::deque<std::pair<unsigned int, unsigned int> > &pairs)
+{
+	std::sort(pairs.begin(), pairs.end(), comparePairs);
+}
+
+bool PmergeMe::checkSortedPair(std::deque<std::pair<unsigned int, unsigned int> > &pairs)
+{
+	for (size_t i = 0; i < pairs.size() - 1; ++i)
+	{
+		if (pairs[i].second < pairs[i + 1].second)
+		{
+			// std::cout << "not sorted" << std::endl;
+			return (false);
+		}
+	}
+	// std::cout << "sorted" << std::endl;
+	return (true);
+
+}
+
+void PmergeMe::mergeInsertion(std::deque<std::pair<unsigned int,unsigned int> > &pairs, std::deque<unsigned int> &dequeStack)
+{
+	unsigned int	odd;
+	bool	isOdd = false;
+	unsigned int	prevJacNumber = 1;
+	unsigned int	jacNumber = 1;
+	if (dequeStack.size() % 2 == 1)
+	{
+		odd = dequeStack.back();
+		isOdd = true;
+	}
+	dequeStack.clear();
+	(void)pairs;
+	// make a chain
+	dequeStack.push_back(pairs[0].first);
+	for (size_t i = 0; i < pairs.size(); ++i)
+	{
+		dequeStack.push_back(pairs[i].second);
+	}
+	// printStack(dequeStack);
+	// std::cout << "pairs size number: " << pairs.size() << std::endl;
+	
+	// insert with Jacobsthal numbers
+	// std::deque<unsigned int>::iterator it;
+	// it = std::find(dequeStack.begin(), dequeStack.end(), pairs[2].second);
+	// std::cout << "iterator find number: " << *it << std::endl;
+	for (int i = pairs.size(); i > 0; i--)
+	{
+		setNextJacNumber(prevJacNumber, jacNumber);
+		// std::cout << "jacobsthal number: " << jacNumber << std::endl;
+		for (unsigned int j = jacNumber - 1; j + 1 > prevJacNumber; j--)
+		{
+			if (j >= pairs.size())
+				continue;
+			// std::cout << "j number: " << j << " i number: " << i << " pairs[j] first: " << pairs[j].first << " pairs[j] second: " << pairs[j].second << std::endl;
+			dequeStack.insert(std::lower_bound(dequeStack.begin(), std::find(dequeStack.begin(), dequeStack.end(), pairs[j].second), pairs[j].first), pairs[j].first);
+			i--;
+		}
+	}
+	if (isOdd)
+	{
+			dequeStack.insert(std::lower_bound(dequeStack.begin(), dequeStack.end(), odd), odd);
+	}
+	// printStack(dequeStack);
+}
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:03:36 by dnebatz           #+#    #+#             */
-/*   Updated: 2024/03/26 22:33:30 by dnebatz          ###   ########.fr       */
+/*   Updated: 2024/03/27 08:45:25 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,13 @@ class PmergeMe
 		static bool comparePairs(const std::pair<int, int>& a, const std::pair<int, int>& b);
 		void mergeInsertion(std::vector<std::pair<unsigned int,unsigned int> > &pairs, std::vector<unsigned int> &vectorStack);
 		void setNextJacNumber(unsigned int &prevJacNumber, unsigned int &jacNumber);
+		
+		void printStack(std::deque<unsigned int> &stack);
+		void printPairs(std::deque<std::pair<unsigned int, unsigned int> > &pairs);
+		void sortEachPair(std::deque<std::pair<unsigned int, unsigned int> > &pairs);
+		void sortPairs(std::deque<std::pair<unsigned int, unsigned int> > &pairs);
+		bool checkSortedPair(std::deque<std::pair<unsigned int, unsigned int> > &pairs);
+		void mergeInsertion(std::deque<std::pair<unsigned int,unsigned int> > &pairs, std::deque<unsigned int> &dequeStack);
 };
 
 #endif
